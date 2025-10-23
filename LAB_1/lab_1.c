32a52916e06c244fec9321eae87ddf69f34ccc4e
Md Tausif Bin Mozid <b.tausif15@gamil.com>
2025-10-23 20:55:43 +0600

    LAB 2 DONE

---
+++ LAB_1/lab_1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int value =1;
+    printf("VaLUE IS  %d ", value);
+    return 0;
+}
---
+++ LAB_2/input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    eeffgg
+    12345
+    eeffgg
+    efg
+}
